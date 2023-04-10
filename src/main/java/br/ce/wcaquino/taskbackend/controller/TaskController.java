698329ee0ba1cb2b9d2d40f37a0c03e310ac61af
vinicius.jacques <vinicius.jacques@compasso.com.br>
2023-04-10 15:17:47 -0300

    adicionando funcionalidade de remocao no backend

--- src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
+++ src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
@@ -46,7 +46,7 @@ public class TaskController {
 		return new ResponseEntity<Task>(saved, HttpStatus.CREATED);
 	}
 	
-	@DeleteMapping(value = "/{id")
+	@DeleteMapping(value = "/{id}")
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
 		todoRepo.deleteById(id);
